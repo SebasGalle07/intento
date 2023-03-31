5aa7c2cf5a9907121da0ee19b1200e590923b3bc
SebasGalle07 <sebastian.gallegos@uqvirtual.edu.co>
2023-03-30 21:03:36 -0500

    1er commit: se creo la clase persona y correo 2do commit : se creo base constructor

---
+++ pruebasGit/src/pruebasGit/gitHub.java
@@ -0,0 +1,16 @@
+package pruebasGit;
+
+public class gitHub {
+	private String nombre;
+	private String correo;
+	
+	public gitHub(String nombre, String correo) {
+		this.nombre = nombre;
+		this.correo = correo;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
